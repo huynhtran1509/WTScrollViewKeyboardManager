0ec02b40375a800784b4787ab3a04718da5764d6
Andrew Carter <andrew.r.carter@gmail.com>
2013-10-07 14:59:05 -0400

    first commit

---
+++ WTScrollViewKeyboardManager/WTRootViewController.h
@@ -0,0 +1,14 @@
+//
+//  WTRootViewController.h
+//  WTScrollViewKeyboardManager
+//
+//  Created by Andrew Carter on 10/7/13.
+//  Copyright (c) 2013 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTRootViewController : UIViewController
+
+
+@end
---
+++ WTScrollViewKeyboardManager/WTRootViewController.m
@@ -0,0 +1,41 @@
+//
+//  WTRootViewController.m
+//  WTScrollViewKeyboardManager
+//
+//  Created by Andrew Carter on 10/7/13.
+//  Copyright (c) 2013 WillowTree Apps. All rights reserved.
+//
+
+#import "WTRootViewController.h"
+
+#import "WTScrollViewKeyboardManager.h"
+
+@interface WTRootViewController ()
+
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, strong) WTScrollViewKeyboardManager *keyboardManager;
+
+@end
+
+@implementation WTRootViewController
+
+#pragma mark - UIViewController Overrides
+
+- (void)viewDidLoad
+{
+    [self setupKeyboardManager];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [[self scrollView] setContentSize:CGSizeMake(CGRectGetWidth([[self scrollView] bounds]), CGRectGetHeight([[self scrollView] bounds]))];
+}
+
+#pragma mark - Instance Methods
+
+- (void)setupKeyboardManager
+{
+    [self setKeyboardManager:[[WTScrollViewKeyboardManager alloc] initWithScrollView:[self scrollView] viewController:self]];
+}
+
+@end
