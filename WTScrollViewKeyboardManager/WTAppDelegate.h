665a81bc885e5be06312fb2a89954ec5c987fedd
Andrew Carter <andrew.r.carter@gmail.com>
2013-10-07 14:08:25 -0400

    Initial Commit

---
+++ WTScrollViewKeyboardManager/WTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WTAppDelegate.h
+//  WTScrollViewKeyboardManager
+//
+//  Created by Andrew Carter on 10/7/13.
+//  Copyright (c) 2013 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WTScrollViewKeyboardManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WTScrollViewKeyboardManager
+//
+//  Created by Andrew Carter on 10/7/13.
+//  Copyright (c) 2013 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WTAppDelegate class]));
+    }
+}
